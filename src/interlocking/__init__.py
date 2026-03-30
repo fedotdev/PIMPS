a1da1b38b7ecaf3ec9f2cc97c28f935e251a90bd
fedotdev <fedorbol.mos@gmail.com>
2026-03-30 19:16:52 +0300

    add interl todos

---
+++ src/interlocking/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+# TODO: после реализации engine.py раскомментировать импорты:
+# from src.interlocking.engine import (
+#     InterlockingEngine,
+#     RouteStatus,
+#     EngineError,
+#     RouteConflictError,
+#     RouteNotFoundError,
+# )
+# from src.interlocking.loader import (
+#     load_station,
+#     ConfigError,
+#     StationConfigError,
+# )
+
+# TODO: определить __all__ после раскомментирования:
+# __all__ = [
+#     "InterlockingEngine",
+#     "RouteStatus",
+#     "EngineError",
+#     "RouteConflictError",
+#     "RouteNotFoundError",
+#     "load_station",
+#     "ConfigError",
+#     "StationConfigError",
+# ]
