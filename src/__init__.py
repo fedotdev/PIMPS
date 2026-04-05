3755ce2ccf5a348c32e0415f0009bf250dc01ef0
fedotdev <fedorbol.mos@gmail.com>
2026-04-05 18:43:24 +0300

    add __init__

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from src.interlocking.loader import load_station, StationConfigError, ConfigError
+from src.interlocking.engine import InterlockingEngine
+from src.models import StationConfig, RouteConfig, SwitchConfig, SwitchPosition, RouteType
+
+__version__ = "0.1.0"
+__all__ = [
+    "load_station", "ConfigError", "StationConfigError",
+    "InterlockingEngine",
+    "StationConfig", "RouteConfig", "SwitchConfig",
+    "SwitchPosition", "RouteType",
+]
